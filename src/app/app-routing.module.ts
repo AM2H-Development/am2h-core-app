60fe0aa3163cb9345a5744c2ac883a78389db4d1
Martin Hetschel <martin.hetschel@mach1kart.de>
2022-01-23 16:04:13 +0100

    menu added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicesComponent } from './devices/devices.component';
+import { MeasuresComponent } from './measures/measures.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/settings', pathMatch: 'full' },
+  { path: 'devices', component: DevicesComponent },
+  { path: 'measures', component: MeasuresComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
